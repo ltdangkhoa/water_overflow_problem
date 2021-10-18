e6b8205a6dab795e64b1e07a4f638e1aed02fa31
Kris <khoa.luu@vnappmob.com>
2021-10-18 17:04:34 +0700

    WIP #1 - Calculate glasses

--- water_overflow/main.py
+++ water_overflow/main.py
@@ -3,30 +3,38 @@ def hello_world():
     return 'Hello world'
 
 class Glass:
-    
     def __init__(self, capacity, fill):
         self.capacity = capacity
         self.fill = fill
+
+    def __repr__(self):
+        return str(self.fill)
 
     def isFull(self):
         if self.fill >= self.capacity:
             return True
         return False
 
+    def overFill(self):
+        return self.fill - self.capacity
+
+
 def water_overflow(k, i, j):
     capacity = 250
-    glass = [[Glass(capacity=capacity, fill=0) for i in range(k)] for j in range(k)]
     remain = k*1000
-    glass[0][0] = remain
+    max_row = remain//capacity
+    glass = [[Glass(capacity=capacity, fill=0) for i in range(max_row)] for j in range(max_row)]
+    glass[0][0].fill = remain
     row = 0
-    while row < 5:
+    while remain > capacity:
         for _j in range(row):
             _i = row - 1
             if glass[_i][_j].isFull():
+                over_fill = glass[_i][_j].overFill()
                 glass[_i][_j].fill = capacity
-                remain =  remain - capacity
-                glass[_i + 1][_j].fill = remain / 2
-                glass[_i + 1][_j + 1].fill = remain / 2
+                glass[_i + 1][_j].fill += over_fill / 2
+                glass[_i + 1][_j + 1].fill += over_fill / 2
+                remain -= capacity
 
         row += 1
 
