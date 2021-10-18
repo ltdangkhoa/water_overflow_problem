75811c0faf29a840c6a989bed081d565673d4aa2
Kris <khoa.luu@vnappmob.com>
2021-10-18 16:26:42 +0700

    WIP #1 - object the glass

--- water_overflow/main.py
+++ water_overflow/main.py
@@ -14,21 +14,21 @@ class Glass:
         return False
 
 def water_overflow(k, i, j):
-    glass = [[0 for i in range(k)] for j in range(k)]
-    capacity = 1 # try to solve with capacity as 1 unit for test cases
-    remain = k
+    capacity = 250
+    glass = [[Glass(capacity=capacity, fill=0) for i in range(k)] for j in range(k)]
+    remain = k*1000
     glass[0][0] = remain
     row = 0
     while row < 5:
         for _j in range(row):
             _i = row - 1
-            if glass[_i][_j] >= capacity:
-                glass[_i][_j] = capacity
+            if glass[_i][_j].isFull():
+                glass[_i][_j].fill = capacity
                 remain =  remain - capacity
-                glass[_i + 1][_j] = remain / 2
-                glass[_i + 1][_j + 1] = remain / 2
+                glass[_i + 1][_j].fill = remain / 2
+                glass[_i + 1][_j + 1].fill = remain / 2
 
         row += 1
 
-    return glass[i][j]
+    return glass[i][j].fill
 
