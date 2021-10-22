19d318c97f1fb614df3cf718060046ca0d4c0177
Kris <khoa.luu@vnappmob.com>
2021-10-22 12:57:47 +0700

    Remove unused row #8

--- tests/test_init.py
+++ tests/test_init.py
@@ -10,6 +10,7 @@ class TestApp(unittest.TestCase):
     def test_water_overflow(self):
         #  self.assertEqual(water_overflow(7, 1, 0), 1)
         #  self.assertEqual(water_overflow(15, 3, 2), 1)
+
         glasses = water_overflow(1)
         self.assertEqual(glasses[2][0].fill, 62.5)
         self.assertEqual(glasses[2][1].fill, 125)
@@ -22,7 +23,9 @@ class TestApp(unittest.TestCase):
         last_row = glasses[len(glasses) - 1]
         have_water = False
         for glass in last_row:
-            have_water = True if glass.fill > 0 else False
+            if glass.fill > 0:
+                have_water = True
+                break
 
         self.assertTrue(have_water)
 
